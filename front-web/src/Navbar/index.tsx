ff59a73fc973261724f525571280d8e089e3b07e
adilson <abrito10@gmail.com>
2021-01-06 15:55:04 -0300

    Add Home Page

--- front-web/src/Navbar/index.tsx
+++ front-web/src/Navbar/index.tsx
@@ -6,7 +6,7 @@ function Navbar(){
 
         <nav className="main-navbar">
             <Logo />
-            <a href = "home" className = "logo-text">DS Delivery</a>
+            <a href = "home" className= "logo-text">DS Delivery</a>
 
         </nav>
 
